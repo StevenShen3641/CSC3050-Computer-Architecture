20a9e00a53f5edf5dc2490e7f243ad975aec4a8a
StevenShen3641 <stevenshen3641@outlook.com>
2023-09-25 18:57:40 +0800

    add files

--- Assignment/Assignment1/CSC3050_P1/phase1.py
+++ Assignment/Assignment1/CSC3050_P1/phase1.py
@@ -10,32 +10,27 @@ def label_detect(file):
     :return: A dict containing label-address pair
     """
     label_addresses = {}
-    if os.path.exists(file):
-        f = open(file, "r")
-        f.seek(0, 2)
-        eof = f.tell()
-        f.seek(0, 0)
-        while True:
-            line = f.readline()
-            if line.find(".text") != -1:
-                break
-        address = START_ADDR
-        while True:
-            line = f.readline()
-            line = line.replace("\n", "")
-            if line.find("#") != -1:
-                line = line.split("#")[0]
-            # end of file
-            if f.tell() >= eof:
-                break
-            if line.find(":") != -1:
-                label = line.split(":")[0].replace(" ", "").replace("\t", "")
-                label_addresses[label] = address
-            # check whether address should be increased
-            joined_line = line.replace(" ", "").replace("\t", "")
-            if len(joined_line) != 0 and joined_line[-1] != ":":
-                address += 1
-        f.close()
-        return label_addresses
-    else:
-        return
+    file.seek(0, 2)
+    eof = file.tell()
+    file.seek(0, 0)
+    while True:
+        line = file.readline()
+        if line.find(".text") != -1:
+            break
+    address = START_ADDR
+    while True:
+        line = file.readline()
+        line = line.replace("\n", "")
+        if line.find("#") != -1:
+            line = line.split("#")[0]
+        # end of file
+        if file.tell() >= eof:
+            break
+        if line.find(":") != -1:
+            label = line.split(":")[0].replace(" ", "").replace("\t", "")
+            label_addresses[label] = address
+        # check whether address should be increased
+        joined_line = line.replace(" ", "").replace("\t", "")
+        if len(joined_line) != 0 and joined_line[-1] != ":":
+            address += 1
+    return label_addresses
--- Assignment/Assignment1/CSC3050_P1/phase2.py
+++ Assignment/Assignment1/CSC3050_P1/phase2.py
@@ -1,8 +1,39 @@
+import sys
+import os
 from phase1 import label_detect
 from labelTabel import REGS, INST_I, INST_J, INST_R, RType, IType
 
+
 # def
 
+def main():
+    inp = sys.argv[1]
+    out = sys.argv[2]
+    outfile = open(out, "w")
+    if os.path.exists(inp):
+        f = open(inp, "r")
+        labels = label_detect(f)
+        f.seek(0, 2)
+        eof = f.tell()
+        f.seek(0, 0)
+        while True:
+            line = f.readline()
+            if line.find(".text") != -1:
+                break
+        while True:
+            line = f.readline()
+            code = parse(line)
+            outfile.write(code)
+            if outfile.tell() >= eof:
+                break
+            else:
+                outfile.write("\n")
+    else:
+        return
+
+def parse(line):
+    return "1"
+
+
 if __name__ == '__main__':
-    infile = "testfile3.asm"
-    print(label_detect(infile))
+    main()
