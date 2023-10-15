9f4346047bac785d8e680a1aacbb9aed22616a46
StevenShen3641 <stevenshen3641@outlook.com>
2023-10-15 14:14:56 +0800

    modify

--- Assignment/Assignment1/CSC3050_P1/testfile4.asm
+++ Assignment/Assignment1/CSC3050_P1/testfile4.asm
@@ -1,36 +1,34 @@
-    .text
+.text  #wqer
 main:
-#a:
-#x:
-    add     $sp, $sp, $sp#
-    addu    $sp, $sp, $sp#
-    div     $sp, $sp#
-    divu    $sp, $sp  #
-    jalr    $sp, $sp        #saffs
-    jr      $sp#  sdfwq
-    mfhi    $sp             #  fsa
-    a:# asdf
+    add     $sp, $sp, $sp
+    addu    $sp, $sp, $sp
+    div     $sp, $sp
+    divu    $sp, $sp
+    jalr    $sp, $sp
+    jr      $sp
+    mfhi    $sp
+    a:
     mflo    $sp
     mthi    $sp
     mtlo    $sp
     mult    $sp, $sp
     multu   $sp, $sp
-    nor     $sp, $sp,$sp
-    or      $sp,$sp, $sp
-    b:  #   qwre
-    sll     $sp, $sp,  10
-    sllv $sp,$sp,$sp
+    nor     $sp, $sp, $sp
+    or      $sp, $sp, $sp
+    b:
+    sll     $sp, $sp, 10
+    sllv    $sp, $sp, $sp
     slt     $sp, $sp, $sp
-    sltu    $sp,  $sp, $sp
-    sra     $sp,  $sp, 8
+    sltu    $sp, $sp, $sp
+    sra     $sp, $sp, 8
     srav    $sp, $sp, $sp
-    srl     $sp,   $sp,  7
-    srlv    $sp, $sp,  $sp
-    sub     $sp,    $sp, $sp
+    srl     $sp, $sp, 7
+    srlv    $sp, $sp, $sp
+    sub     $sp, $sp, $sp
     subu    $sp, $sp, $sp
     syscall
     xor     $sp, $zero, $sp
-    addi    $sp, $sp, -44
+    addi    $sp, $sp, -32768
     addiu   $sp, $sp, 98
     andi    $sp, $sp, 48
     beq     $sp, $sp, a
@@ -39,17 +37,17 @@ main:
     bgtz    $sp, c
     lb      $sp, 7($sp)
     lbu     $sp, 0($sp)
-    lh     $sp, 15($sp)
+    lh      $sp, 15($sp)
     lhu     $sp, 9($sp)
-    lui $sp, 89
-    lw  $sp, 19 ($sp)
-    ori     $sp, $sp,  9
-    sb  $sp,87( $sp)
+    lui     $sp, 89
+    lw      $sp, 19($sp)
+    ori     $sp, $sp, 9
+    sb      $sp, 87($sp)
     slti    $sp, $sp, 121
-    sltiu       $sp, $sp,124
-    sh      $sp, 23($sp )
+    sltiu   $sp, $sp, 124
+    sh      $sp, 23($sp)
     xori    $sp, $sp, 1
-    lwl $sp, 1($sp)
+    lwl     $sp, 1($sp)
     lwr     $sp, 5($sp)
     swl     $sp, 3($sp)
     swr     $sp, 9($sp)
