625fff8ff7a1cf332461a50d55fe8ba8e84d0266
StevenShen3641 <stevenshen3641@outlook.com>
2023-10-17 19:26:13 +0800

    modify

--- Assignment/Assignment2/CSC3050_P2/cmake-build-debug/CMakeFiles/3.26.4/CMakeRCCompiler.cmake
+++ Assignment/Assignment2/CSC3050_P2/cmake-build-debug/CMakeFiles/3.26.4/CMakeRCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_RC_COMPILER "D:/program files/JetBrains/CLion 2022.1.2/bin/mingw/bin/windres.exe")
+set(CMAKE_RC_COMPILER "D:/mingw64/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
