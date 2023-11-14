305a2bdd0533425d50590980e284f672f2d523d0
StevenShen3641 <stevenshen3641@outlook.com>
2023-11-14 20:42:51 +0800

    modify

--- Assignment/Assignment 3/CSC3050_P3/alu.v
+++ Assignment/Assignment 3/CSC3050_P3/alu.v
@@ -5,18 +5,20 @@
 // regA: 00000; regB: 00001
 
 module alu(
-    input [31:0] instruction,
-    input [31:0] regA,
-    input [31:0] regB,
-    output reg [31:0] result,
-    output reg [2:0] flags
+    input[31:0] instruction,
+    input[31:0] regA,
+    input[31:0] regB,
+    output reg[31:0] result,
+    output reg[2:0] flags
 );
 
-    wire [5:0] opcode;
-    reg [5:0] funct;
-    reg [4:0] rs, rt, rd, shamt;
-    reg [15:0] immediate;
-    reg [31:0] rs_reg, rt_reg;
+    wire[5:0] opcode;
+    reg[5:0] funct;
+    reg[4:0] rs, rt, rd, shamt;
+    reg[15:0] immediate;
+    reg[31:0] rs_reg, rt_reg;
+
+    reg[31:0] temp_reg;  // for slti and sltiu
 
     assign opcode = instruction[31:26];
 
@@ -30,7 +32,7 @@ module alu(
             shamt = instruction[10:6];
             funct = instruction[5:0];
         end
-        // I-type
+            // I-type
         else begin
             rs = instruction[25:21];
             rt = instruction[20:16];
@@ -54,6 +56,7 @@ module alu(
     // exicute different instructions
     always @(*) begin
         flags = 3'b0;  // default the flag
+        result = 32'b0;  // default the result registers
         case (opcode)
             // R-type
             6'h00: begin
@@ -76,10 +79,24 @@ module alu(
                     6'h04:  // sllv
                         result = rt_reg << rs_reg;
                     6'h2A: begin  // slt
-                       // result =
+                        if ($signed(rs_reg-rt_reg) < 0) begin
+                            flags[1] = 1'b1;
+                            result = 32'b1;
+                        end
+                        else begin
+                            flags[1] = 1'b0;
+                            result = 32'b0;
+                        end
                     end
                     6'h2B: begin  // sltu
-                       // result =
+                        if (rs_reg < rt_reg) begin
+                            flags[1] = 1'b1;
+                            result = 32'b1;
+                        end
+                        else begin
+                            flags[1] = 1'b0;
+                            result = 32'b0;
+                        end
                     end
                     6'h03:  // sra
                         result = $signed(rt_reg) >>> shamt;
@@ -90,22 +107,21 @@ module alu(
                     6'h06:  // srlv
                         result = rt_reg >> rs_reg;
                     6'h22: begin  // sub
-                        result = $signed(rs_reg) - $signed(rt_reg);
+                        result = $signed(rs_reg)-$signed(rt_reg);
                         if ({result[31], rs_reg[31], rt_reg[31]} == {3'b101} || {result[31], rs_reg[31], rt_reg[31]} == {3'b010})
                             flags[0] = 1;
                     end
                     6'h23:  // subu
-                        result = rs_reg - rt_reg;
+                        result = rs_reg-rt_reg;
                     6'h26:  // xor
                         result = rs_reg ^ rt_reg;
                 endcase
             end
 
             // I-type
-            // addiu is implemented follows as an example, you should implement others by yourself.
             6'h08: begin  // addi
                 result = rs_reg+{{(16){immediate[15]}}, immediate};
-                if ({result[31], immediate[15], rs_reg[31]}=={3'b100} || {result[31], immediate[15], rs_reg[31]}=={3'b011})
+                if ({result[31], immediate[15], rs_reg[31]} == {3'b100} || {result[31], immediate[15], rs_reg[31]} == {3'b011})
                     flags[0] = 1;
             end
             6'h09:  // addiu
@@ -113,20 +129,44 @@ module alu(
             6'h0C:  // andi
                 result = rs_reg & {16'b0, immediate};
             6'h04: begin  // beq
-               //
+                result = rs_reg-rt_reg;
+                if (result == 32'b0)
+                    flags[2] = 1'b1;
+                else
+                    flags[2] = 1'b0;
             end
             6'h05: begin  // bne
-                //
+                result = rs_reg-rt_reg;
+                if (result == 32'b0)
+                    flags[2] = 1'b1;
+                else
+                    flags[2] = 1'b0;
             end
             6'h23:  // lw
                 result = rs_reg+{{(16){immediate[15]}}, immediate};
             6'h0D:  // ori
                 result = rs_reg | {16'b0, immediate};
             6'h0A: begin  // slti
-                //
+                temp_reg = {{(16){immediate[15]}}, immediate};
+                if ($signed(rs_reg-temp_reg) < 0) begin
+                    flags[1] = 1'b1;
+                    result = 32'b1;
+                end
+                else begin
+                    flags[1] = 1'b0;
+                    result = 32'b0;
+                end
             end
             6'h0B: begin  // sltiu
-                //
+                temp_reg = {{(16){immediate[15]}}, immediate};
+                if (rs_reg < temp_reg) begin
+                    flags[1] = 1'b1;
+                    result = 32'b1;
+                end
+                else begin
+                    flags[1] = 1'b0;
+                    result = 32'b0;
+                end
             end
             6'h2B:  // sw
                 result = rs_reg+{{(16){immediate[15]}}, immediate};
--- Assignment/Assignment 3/CSC3050_P3/test.v
+++ Assignment/Assignment 3/CSC3050_P3/test.v
@@ -1,13 +1,15 @@
 module test_alu();
-    reg signed [3:0] a, b;
     reg [3:0] d, e;
     reg [31:0] c;
     initial begin
-        b = 4'b1100;
-        d = 4'b0111;
-        e = 4'b1001;
-        c = {{16{1'b1}}, 16'b0};
-        $display("%4b", $signed(d) - $signed(e));
-        $display("%4b", d - e);
+        d = 4'b0000;
+        e = 4'b0101;
+        c = 32'b0;
+        if (c == 32'b1) begin
+            $display(1);
+        end
+        else $display(0);
+
+        $display("%32b", c);
     end
 endmodule
